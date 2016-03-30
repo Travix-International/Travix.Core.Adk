9b04b42b227c9fc105582217f3e909478c7084df
GVdP <gdepava@travix.com>
2016-03-30 17:18:42 +0200

    Added vendoring support

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
 # Note: This script hasn't been tested yet and probably needs some tweaking
+
+set -euf -o pipefail
+
+git subtree pull --prefix vendor/gopkg.in/alecthomas/kingpin.v2 https://gopkg.in/alecthomas/kingpin.v2.git master --squash
+
 go build -o appix -i . 
