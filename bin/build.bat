0f815b0a7e207f0d5008b73211aa7ca3389a5183
Ralph Ligtenberg <rligtenberg@travix.com>
2016-03-31 09:05:41 +0200

    Added subtree pull commands to build.bat

--- bin/build.bat
+++ bin/build.bat
@@ -1,4 +1,8 @@
 @echo off
 pushd %~dp0%
-go build -o appix.exe -i ../
+cd ..
+git subtree pull --prefix vendor/gopkg.in/alecthomas/kingpin.v2 https://gopkg.in/alecthomas/kingpin.v2.git master --squash
+git subtree pull --prefix vendor/github.com/alecthomas/template https://github.com/alecthomas/template.git master --squash
+git subtree pull --prefix vendor/github.com/alecthomas/units https://github.com/alecthomas/units.git master --squash
+go build -o bin\appix.exe -i .
 popd
