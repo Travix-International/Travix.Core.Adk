fa725e3b831a6bc958c74d0b51febfd99d0775e9
Ralph Ligtenberg <rligtenberg@travix.com>
2016-04-01 14:38:12 +0200

    Updated build scripts with cross-compiling commands

--- bin/build-all.bat
+++ bin/build-all.bat
@@ -1,22 +1,23 @@
-#!/bin/bash
-# Note: This script hasn't been tested yet and probably needs some tweaking
-
-set -euf -o pipefail
+@echo off
+pushd %~dp0%
+cd ..
 
 git subtree pull --prefix vendor/gopkg.in/alecthomas/kingpin.v2 https://gopkg.in/alecthomas/kingpin.v2.git master --squash
 git subtree pull --prefix vendor/github.com/alecthomas/template https://github.com/alecthomas/template.git master --squash
 git subtree pull --prefix vendor/github.com/alecthomas/units https://github.com/alecthomas/units.git master --squash
 
 set GOARCH=amd64
-set GOOS=windows
-echo Building Windows binary...
-go build -o bin\appix.exe -i .
+set GOOS=linux
+echo Building Linux binary...
+go build -o bin\appix-linux -i .
 
 set GOOS=darwin
 echo Building Mac binary...
 go build -o bin\appix-mac -i .
 
-set GOOS=linux
-echo Building Linux binary...
-go build -o appix -i . 
+set GOOS=windows
+echo Building Windows binary...
+go build -o bin\appix.exe -i .
 
+echo "Done!"
+popd
--- bin/build.bat
+++ bin/build.bat
@@ -4,5 +4,11 @@ cd ..
 git subtree pull --prefix vendor/gopkg.in/alecthomas/kingpin.v2 https://gopkg.in/alecthomas/kingpin.v2.git master --squash
 git subtree pull --prefix vendor/github.com/alecthomas/template https://github.com/alecthomas/template.git master --squash
 git subtree pull --prefix vendor/github.com/alecthomas/units https://github.com/alecthomas/units.git master --squash
+
+set GOARCH=amd64
+set GOOS=windows
+echo Building Windows binary...
 go build -o bin\appix.exe -i .
+
+echo "Done!"
 popd
