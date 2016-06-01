781752825a8c6113bc861c6df934c4753075df88
Mark Vincze <mvincze@travix.com>
2016-06-01 10:10:04 +0200

    TR-10889 Replace exec unzip with proper unzipping with library

--- zipFolder.go
+++ zipFolder.go
@@ -3,9 +3,7 @@ package main
 import (
 	"archive/zip"
 	"io"
-	"io/ioutil"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -79,78 +77,42 @@ func zipFolder(source, target string, includePathInZipFn func(string, bool) bool
 }
 
 func extractZip(src, dest string) error {
-	path, err := exec.LookPath("unzip")
+	reader, err := zip.OpenReader(src)
 
-	if err == nil {
-		err := os.MkdirAll(dest, 0755)
-		if err != nil {
-			return err
-		}
-
-		unzipCmd := exec.Command(path, src)
-		unzipCmd.Dir = dest
-
-		return unzipCmd.Run()
-	} else {
-		files, err := zip.OpenReader(src)
-		if err != nil {
-			return err
-		}
-
-		defer files.Close()
-
-		for _, file := range files.File {
-			err = func() error {
-				readCloser, err := file.Open()
-				if err != nil {
-					return err
-				}
-				defer readCloser.Close()
-
-				return extractZipArchiveFile(file, dest, readCloser)
-			}()
-
-			if err != nil {
-				return err
-			}
-		}
-
-		return nil
+	if err != nil {
+		return err
 	}
-}
 
-func extractZipArchiveFile(file *zip.File, dest string, input io.Reader) error {
-	filePath := filepath.Join(dest, file.Name)
-	fileInfo := file.FileInfo()
+	defer reader.Close()
 
-	if fileInfo.IsDir() {
-		err := os.MkdirAll(filePath, fileInfo.Mode())
-		if err != nil {
-			return err
-		}
-	} else {
-		err := os.MkdirAll(filepath.Dir(filePath), 0755)
+	for _, f := range reader.Reader.File {
+
+		zipped, err := f.Open()
 		if err != nil {
 			return err
 		}
 
-		if fileInfo.Mode()&os.ModeSymlink != 0 {
-			linkName, err := ioutil.ReadAll(input)
+		defer zipped.Close()
+
+		path := filepath.Join(dest, f.Name)
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, 0777)
+		} else {
+			dirPath := filepath.Dir(path)
+			os.MkdirAll(dirPath, 0777)
+
+			writer, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, f.Mode())
+
 			if err != nil {
 				return err
 			}
-			return os.Symlink(string(linkName), filePath)
-		}
 
-		fileCopy, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, fileInfo.Mode())
-		if err != nil {
-			return err
-		}
-		defer fileCopy.Close()
+			defer writer.Close()
 
-		_, err = io.Copy(fileCopy, input)
-		if err != nil {
-			return err
+			if _, err = io.Copy(writer, zipped); err != nil {
+				return err
+			}
 		}
 	}
 
