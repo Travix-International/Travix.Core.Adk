3314831ed89c64cf94c50c157b3169f3bdf23184
Mark Vincze <mvincze@travix.com>
2016-06-01 09:09:18 +0200

    TR-10889 Implement proof of concept scaffolding

--- scaffold.go
+++ scaffold.go
@@ -1,6 +1,14 @@
 package main
 
-import "log"
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
 
 func scaffoldNewApp(appPath string) error {
 	// Apply the template
@@ -31,6 +39,70 @@ func scaffoldNewApp(appPath string) error {
 }
 
 func applyTemplate(appPath string) error {
-	// TODO: apply the actual templating logic.
+	// This is just for the time being, we should upload this file to the Travix Github account.
+	helloWorldTemplateURL := "https://raw.githubusercontent.com/markvincze/jil-playground/master/HelloWorldTemplate.zip"
+
+	tempFolder, err := ioutil.TempDir("", "appix")
+	if err != nil {
+		return err
+	}
+
+	tempFile := filepath.Join(tempFolder, "template.zip")
+	out, err := os.Create(tempFile)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	log.Println("Downloading template from " + helloWorldTemplateURL)
+
+	// Download the template.
+	resp, err := http.Get(helloWorldTemplateURL)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	// Unzip the template and do some basic replacements.
+	err = extractZip(tempFile, appPath)
+	if err != nil {
+		return err
+	}
+
+	appName := filepath.Base(appPath)
+
+	// This is just a temporary, proof of concept implementation, we'll need a proper solution for scaffolding and templating.
+	if err = replaceInFile(filepath.Join(appPath, "app.manifest"), "{APP_NAME}", appName); err != nil {
+		return err
+	}
+
+	if err = replaceInFile(filepath.Join(appPath, "ui/helloWorldWidget/src/component.js"), "{APP_NAME}", appName); err != nil {
+		return err
+	}
+
+	if err = replaceInFile(filepath.Join(appPath, "ui/helloWorldWidget/src/index.js"), "{APP_NAME}", appName); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func replaceInFile(file, from, to string) error {
+	input, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	output := bytes.Replace(input, []byte(from), []byte(to), -1)
+
+	err = ioutil.WriteFile(file, output, 0666)
+
+	return err
 }
--- zipFolder.go
+++ zipFolder.go
@@ -3,7 +3,9 @@ package main
 import (
 	"archive/zip"
 	"io"
+	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -74,4 +76,83 @@ func zipFolder(source, target string, includePathInZipFn func(string, bool) bool
 	})
 
 	return err
+}
+
+func extractZip(src, dest string) error {
+	path, err := exec.LookPath("unzip")
+
+	if err == nil {
+		err := os.MkdirAll(dest, 0755)
+		if err != nil {
+			return err
+		}
+
+		unzipCmd := exec.Command(path, src)
+		unzipCmd.Dir = dest
+
+		return unzipCmd.Run()
+	} else {
+		files, err := zip.OpenReader(src)
+		if err != nil {
+			return err
+		}
+
+		defer files.Close()
+
+		for _, file := range files.File {
+			err = func() error {
+				readCloser, err := file.Open()
+				if err != nil {
+					return err
+				}
+				defer readCloser.Close()
+
+				return extractZipArchiveFile(file, dest, readCloser)
+			}()
+
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+}
+
+func extractZipArchiveFile(file *zip.File, dest string, input io.Reader) error {
+	filePath := filepath.Join(dest, file.Name)
+	fileInfo := file.FileInfo()
+
+	if fileInfo.IsDir() {
+		err := os.MkdirAll(filePath, fileInfo.Mode())
+		if err != nil {
+			return err
+		}
+	} else {
+		err := os.MkdirAll(filepath.Dir(filePath), 0755)
+		if err != nil {
+			return err
+		}
+
+		if fileInfo.Mode()&os.ModeSymlink != 0 {
+			linkName, err := ioutil.ReadAll(input)
+			if err != nil {
+				return err
+			}
+			return os.Symlink(string(linkName), filePath)
+		}
+
+		fileCopy, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, fileInfo.Mode())
+		if err != nil {
+			return err
+		}
+		defer fileCopy.Close()
+
+		_, err = io.Copy(fileCopy, input)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
