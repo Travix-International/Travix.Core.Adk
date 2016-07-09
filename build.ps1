59b6685f2a51964b459892b17b5d0462b3517a4c
Mark Vincze <mrk.vincze@gmail.com>
2016-07-09 21:31:47 +0200

    Add quotes to platform strings

--- build.ps1
+++ build.ps1
@@ -3,16 +3,16 @@ Write-Output "Retrieving/updating vendor packages using GVT..."
 gvt update gopkg.in/alecthomas/kingpin.v2
 gvt update github.com/nu7hatch/gouuid
 
-$env:GOARCH=amd64
-$env:GOOS=linux
+$env:GOARCH="amd64"
+$env:GOOS="linux"
 Write-Output "Building Linux binary..."
 go build -o bin\appix-linux -i .
 
-$env:GOOS=darwin
+$env:GOOS="darwin"
 Write-Output "Building Mac binary..."
 go build -o bin\appix-mac -i .
 
-$env:GOOS=windows
+$env:GOOS="windows"
 Write-Output "Building Windows binary..."
 go build -o bin\appix.exe -i .
 
