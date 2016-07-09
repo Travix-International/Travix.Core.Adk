36af55172462ccb33bf918811b00bba5364f1f72
Mark Vincze <mrk.vincze@gmail.com>
2016-07-09 21:14:45 +0200

    Add build PS script and appveyor.yml

---
+++ build.ps1
@@ -0,0 +1,19 @@
+Write-Output "Retrieving/updating vendor packages using GVT..."
+
+gvt update gopkg.in/alecthomas/kingpin.v2 || true
+gvt update github.com/nu7hatch/gouuid || true
+
+$env:GOARCH=amd64
+$env:GOOS=linux
+Write-Output "Building Linux binary..."
+go build -o bin\appix-linux -i .
+
+$env:GOOS=darwin
+Write-Output "Building Mac binary..."
+go build -o bin\appix-mac -i .
+
+$env:GOOS=windows
+Write-Output "Building Windows binary..."
+go build -o bin\appix.exe -i .
+
+Write-Output "Done!"
