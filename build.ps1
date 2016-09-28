d7e352b6340153a342d6eaab04634e4e21fb8b23
jvantillo <jvantillo@travix.com>
2016-09-28 18:19:08 +0200

    script debugging 2

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 Write-Output "Building the appix binaries..."
 
 if (Test-Path env:APPVEYOR_BUILD_VERSION) {
-    $BUILD_VERSION = $APPVEYOR_BUILD_VERSION
-    $GIT_HASH = $APPVEYOR_REPO_COMMIT
+    $BUILD_VERSION = $env:APPVEYOR_BUILD_VERSION
+    $GIT_HASH = $env:APPVEYOR_REPO_COMMIT
 } else {
     $BUILD_VERSION = "0.0.0"
     $GIT_HASH = "000"
@@ -10,7 +10,7 @@ if (Test-Path env:APPVEYOR_BUILD_VERSION) {
 $BUILD_DATE = (Get-Date -Date ((Get-Date).ToUniversalTime()) -UFormat %a.%B.%d.%Y.%R:%S) + ".+0000.UTC"
 $APP_LDFLAGS="-s -X main.version=$BUILD_VERSION -X main.gitHash=$GIT_HASH -X main.buildDate=$BUILD_DATE"
 Write-Output "Load flags will be $APP_LDFLAGS"
-Write-Output "AV vars are $APPVEYOR_BUILD_VERSION / $APPVEYOR_REPO_COMMIT"
+Write-Output "AV vars are $env:APPVEYOR_BUILD_VERSION / $env:APPVEYOR_REPO_COMMIT"
 
 $env:GOARCH="amd64"
 $env:GOOS="linux"
