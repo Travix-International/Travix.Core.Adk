909e1ef7a5d404bf12e5ba346410e7a7bec8c90b
jvantillo <jvantillo@travix.com>
2016-09-28 17:46:35 +0200

    Added version command plus build changes

--- build.ps1
+++ build.ps1
@@ -1,16 +1,27 @@
 Write-Output "Building the appix binaries..."
 
+if (Test-Path env:APPVEYOR_BUILD_VERSION) {
+    $BUILD_VERSION = $APPVEYOR_BUILD_VERSION
+    $GIT_HASH = $APPVEYOR_REPO_COMMIT
+} else {
+    $BUILD_VERSION = "0.0.0"
+    $GIT_HASH = "000"
+}
+$BUILD_DATE = Get-Date -UFormat "%a.%B.%d.%Y.%R:%S.%z.%Z"
+$APP_LDFLAGS="-s -X main.version=$BUILD_VERSION -X main.gitHash=$GIT_HASH -X main.buildDate=$BUILD_DATE"
+
+
 $env:GOARCH="amd64"
 $env:GOOS="linux"
 Write-Output "Building Linux binary..."
-go build -o bin\appix-linux -i .
+go build -ldflags "$APP_LDFLAGS" -o bin\appix-linux -i .
 
 $env:GOOS="darwin"
 Write-Output "Building Mac binary..."
-go build -o bin\appix-mac -i .
+go build -ldflags "$APP_LDFLAGS" -o bin\appix-mac -i .
 
 $env:GOOS="windows"
 Write-Output "Building Windows binary..."
-go build -o bin\appix.exe -i .
+go build -ldflags "$APP_LDFLAGS" -o bin\appix.exe -i .
 
 Write-Output "Done!"
---
+++ version.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+func configureVersionCommand(app *kingpin.Application) {
+	app.Command("version", "Displays version information").
+		Action(executeVersionCommand).
+		Alias("ver")
+}
+
+func executeVersionCommand(context *kingpin.ParseContext) error {
+	log.Printf("Version: %s", version)
+	log.Printf("Hash: %s", gitHash)
+	log.Printf("Build date: %s", parsedBuildDate)
+	return nil
+}
