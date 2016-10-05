5779e6d935a681a76bb105b72b4cacfea2bc78a7
Mark Vincze <mrk.vincze@gmail.com>
2016-10-05 19:27:45 +0200

    Remove the echos

--- build.sh
+++ build.sh
@@ -7,11 +7,6 @@ BUILD_DATE=`LC_ALL=en_US.utf8 date -u +"%a.%B.%d.%Y.%R:%S.%z.%Z"`
 : "${TRAVIS_COMMIT:=`git rev-parse --short HEAD`}"
 APP_LDFLAGS="-s -X main.version=$TRAVIS_TAG -X main.gitHash=$TRAVIS_COMMIT -X main.buildDate=$BUILD_DATE"
 
-echo "TRAVIS_BUILD_ID: $TRAVIS_BUILD_ID"
-echo "TRAVIS_BUILD_NUMBER: $TRAVIS_BUILD_NUMBER"
-echo "TRAVIS_JOB_NUMBER: $TRAVIS_JOB_NUMBER"
-echo "TRAVIS_COMMIT: $TRAVIS_COMMIT"
-
 echo "Building Windows binary..."
 GOARCH=amd64 GOOS=windows go build -ldflags "$APP_LDFLAGS" -o bin/appix.exe -i .
 
