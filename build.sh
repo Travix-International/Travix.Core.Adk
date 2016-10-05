6b46b3cf8cf10040508b179d9bba168012bc7d98
Mark Vincze <mrk.vincze@gmail.com>
2016-10-05 19:23:13 +0200

    Use Travis env variables

--- build.sh
+++ build.sh
@@ -3,9 +3,14 @@
 set -euf -o pipefail
 
 BUILD_DATE=`LC_ALL=en_US.utf8 date -u +"%a.%B.%d.%Y.%R:%S.%z.%Z"`
-: "${APPVEYOR_BUILD_VERSION:=0.0.0}"
-: "${APPVEYOR_REPO_COMMIT:=`git rev-parse --short HEAD`}"
-APP_LDFLAGS="-s -X main.version=$APPVEYOR_BUILD_VERSION -X main.gitHash=$APPVEYOR_REPO_COMMIT -X main.buildDate=$BUILD_DATE"
+: "${TRAVIS_TAG:=0.0.0}"
+: "${TRAVIS_COMMIT:=`git rev-parse --short HEAD`}"
+APP_LDFLAGS="-s -X main.version=$TRAVIS_TAG -X main.gitHash=$TRAVIS_COMMIT -X main.buildDate=$BUILD_DATE"
+
+echo "TRAVIS_BUILD_ID: $TRAVIS_BUILD_ID"
+echo "TRAVIS_BUILD_NUMBER: $TRAVIS_BUILD_NUMBER"
+echo "TRAVIS_JOB_NUMBER: $TRAVIS_JOB_NUMBER"
+echo "TRAVIS_COMMIT: $TRAVIS_COMMIT"
 
 echo "Building Windows binary..."
 GOARCH=amd64 GOOS=windows go build -ldflags "$APP_LDFLAGS" -o bin/appix.exe -i .
