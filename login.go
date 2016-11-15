200e6dcb1e53da32a9cc542d837780c337f8d802
Fahad Ibnay Heylaal <fahad19@gmail.com>
2016-11-15 12:52:43 +0100

    start and stop server, via a channel.

--- login.go
+++ login.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
@@ -13,5 +15,39 @@ func configureLoginCommand(app *kingpin.Application) {
 
 func executeLoginCommand(context *kingpin.ParseContext) error {
 	fmt.Println("Login here...")
+	ch := make(chan bool)
+
+	go startLoginServer(ch)
+
+	openWebsite("http://localhost:7001")
+
+	select {
+	case shouldClose := <-ch:
+		if shouldClose {
+
+		} else {
+
+		}
+	}
+
 	return nil
 }
+
+func loginBaseRoute(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world from login server!")
+}
+
+func startLoginServer(c chan bool) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello world from login server!")
+	})
+
+	http.HandleFunc("/close", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "closing...")
+
+		c <- true
+	})
+
+	http.ListenAndServe(":7001", nil)
+}
+
