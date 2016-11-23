8a456c9e8d6f59cd3c7b525652b1b8de1ad8aca8
Fahad Ibnay Heylaal <fahad19@gmail.com>
2016-11-23 11:28:36 +0100

    break arguments into multiple lines.

--- build.sh
+++ build.sh
@@ -5,7 +5,16 @@ set -euf -o pipefail
 BUILD_DATE=`LC_ALL=en_US.utf8 date -u +"%a.%B.%d.%Y.%R:%S.%z.%Z"`
 : "${TRAVIS_TAG:=0.0.0}"
 : "${TRAVIS_COMMIT:=`git rev-parse --short HEAD`}"
-APP_LDFLAGS="-s -X main.version=$TRAVIS_TAG -X main.gitHash=$TRAVIS_COMMIT -X main.buildDate=$BUILD_DATE -X main.travixFirebaseApiKey=$TRAVIX_FIREBASE_API_KEY -X main.travixFirebaseAuthDomain=$TRAVIX_FIREBASE_AUTH_DOMAIN -X main.travixFirebaseDatabaseUrl=$TRAVIX_FIREBASE_DATABASE_URL -X main.travixFirebaseStorageBucket=$TRAVIX_FIREBASE_STORAGE_BUCKET -X main.travixFirebaseMessagingSenderId=$TRAVIX_FIREBASE_MESSAGING_SENDER_ID -X main.travixDeveloperProfileUrl=$TRAVIX_DEVELOPER_PROFILE_URL"
+APP_LDFLAGS="-s
+-X main.version=$TRAVIS_TAG
+-X main.gitHash=$TRAVIS_COMMIT
+-X main.buildDate=$BUILD_DATE
+-X main.travixFirebaseApiKey=$TRAVIX_FIREBASE_API_KEY
+-X main.travixFirebaseAuthDomain=$TRAVIX_FIREBASE_AUTH_DOMAIN
+-X main.travixFirebaseDatabaseUrl=$TRAVIX_FIREBASE_DATABASE_URL
+-X main.travixFirebaseStorageBucket=$TRAVIX_FIREBASE_STORAGE_BUCKET
+-X main.travixFirebaseMessagingSenderId=$TRAVIX_FIREBASE_MESSAGING_SENDER_ID
+-X main.travixDeveloperProfileUrl=$TRAVIX_DEVELOPER_PROFILE_URL"
 
 echo "Building Windows binary..."
 GOARCH=amd64 GOOS=windows go build -ldflags "$APP_LDFLAGS" -o bin/appix.exe -i .
