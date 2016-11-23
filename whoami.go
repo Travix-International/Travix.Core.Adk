42ad326c75e1d0cbc1f13841890a99867fc5b66f
Fahad Ibnay Heylaal <fahad19@gmail.com>
2016-11-23 13:22:17 +0100

    break fetching of refreshed token and developer profile into two functions.

--- whoami.go
+++ whoami.go
@@ -10,6 +10,74 @@ import (
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
+type TokenBody struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    string `json:"expires_in"`
+	IdToken      string `json:"id_token"`
+	ProjectId    string `json:"project_id"`
+	RefreshToken string `json:"refresh_token"`
+	TokenType    string `json:"token_type"`
+	UserId       string `json:"user_id"`
+}
+
+type Profile struct {
+	Email          string
+	FirebaseUserId string
+	Id             int
+	IsEnabled      bool
+	IsVerified     bool
+	Name           string
+	PublisherId    string
+}
+
+type ProfileBody struct {
+	HasProfile bool
+	Profile    Profile
+}
+
+func fetchRefreshedToken(config Config, refreshToken string) (TokenBody, error) {
+	tokenClient := &http.Client{}
+	var tokenReqPayload = []byte(`{"grant_type":"refresh_token","refresh_token": "` + refreshToken + `"}`)
+	tokenReq, tokenReqErr := http.NewRequest("POST", "https://securetoken.googleapis.com/v1/token?key="+config.FirebaseApiKey, bytes.NewBuffer(tokenReqPayload))
+	if tokenReqErr != nil {
+		return TokenBody{}, tokenReqErr
+	}
+
+	tokenReq.Header.Set("Content-Type", "application/json")
+	tokenRes, tokenResErr := tokenClient.Do(tokenReq)
+	if tokenResErr != nil {
+		return TokenBody{}, tokenResErr
+	}
+
+	tokenBody := TokenBody{}
+	json.NewDecoder(tokenRes.Body).Decode(&tokenBody)
+
+	return tokenBody, nil
+}
+
+func fetchDeveloperProfile(config Config, tokenBody TokenBody) (ProfileBody, error) {
+	tokenType := tokenBody.TokenType
+	tokenValue := tokenBody.IdToken
+
+	profileClient := &http.Client{}
+	profileReq, profileReqErr := http.NewRequest("GET", config.DeveloperProfileUrl+"/profile", nil)
+	if profileReqErr != nil {
+		return ProfileBody{}, profileReqErr
+	}
+
+	profileReq.Header.Set("Content-Type", "application/json")
+	profileReq.Header.Set("Authorization", tokenType+" "+tokenValue)
+	profileRes, profileResErr := profileClient.Do(profileReq)
+	if profileResErr != nil {
+		return ProfileBody{}, profileResErr
+	}
+
+	profileBody := ProfileBody{}
+	json.NewDecoder(profileRes.Body).Decode(&profileBody)
+
+	return profileBody, nil
+}
+
 func configureWhoamiCommand(app *kingpin.Application) {
 	app.Command("whoami", "Displays logged in user's information").
 		Action(executeWhoamiCommand)
@@ -18,79 +86,27 @@ func configureWhoamiCommand(app *kingpin.Application) {
 func executeWhoamiCommand(context *kingpin.ParseContext) error {
 	config := GetConfig()
 
+	// get locally stored auth info
 	auth, authErr := GetAuth(config)
 	if authErr != nil {
 		log.Fatal(authErr)
 		return nil
 	}
 
-	refreshToken := auth.User.StsTokenManager.RefreshToken
-
 	// fetch refreshed token
-	type TokenBody struct {
-		AccessToken  string `json:"access_token"`
-		ExpiresIn    string `json:"expires_in"`
-		IdToken      string `json:"id_token"`
-		ProjectId    string `json:"project_id"`
-		RefreshToken string `json:"refresh_token"`
-		TokenType    string `json:"token_type"`
-		UserId       string `json:"user_id"`
-	}
-
-	tokenClient := &http.Client{}
-	var tokenReqPayload = []byte(`{"grant_type":"refresh_token","refresh_token": "` + refreshToken + `"}`)
-	tokenReq, tokenReqErr := http.NewRequest("POST", "https://securetoken.googleapis.com/v1/token?key="+config.FirebaseApiKey, bytes.NewBuffer(tokenReqPayload))
-	if tokenReqErr != nil {
-		log.Fatal(tokenReqErr)
+	refreshToken := auth.User.StsTokenManager.RefreshToken
+	tokenBody, tokenBodyErr := fetchRefreshedToken(config, refreshToken)
+	if tokenBodyErr != nil {
+		log.Fatal(tokenBodyErr)
 		return nil
 	}
-
-	tokenReq.Header.Set("Content-Type", "application/json")
-	tokenRes, tokenResErr := tokenClient.Do(tokenReq)
-	if tokenResErr != nil {
-		log.Fatal(tokenResErr)
-		return nil
-	}
-
-	tokenBody := TokenBody{}
-	json.NewDecoder(tokenRes.Body).Decode(&tokenBody)
-
-	tokenType := tokenBody.TokenType
-	tokenValue := tokenBody.IdToken
 
 	// fetch profile
-	type Profile struct {
-		Email          string
-		FirebaseUserId string
-		Id             int
-		IsEnabled      bool
-		IsVerified     bool
-		Name           string
-		PublisherId    string
-	}
-
-	type ProfileBody struct {
-		HasProfile bool
-		Profile    Profile
-	}
-
-	profileClient := &http.Client{}
-	profileReq, profileReqErr := http.NewRequest("GET", config.DeveloperProfileUrl+"/profile", nil)
-	if profileReqErr != nil {
-		log.Fatal(profileReqErr)
+	profileBody, profileBodyErr := fetchDeveloperProfile(config, tokenBody)
+	if profileBodyErr != nil {
+		log.Fatal(profileBodyErr)
 		return nil
 	}
-
-	profileReq.Header.Set("Content-Type", "application/json")
-	profileReq.Header.Set("Authorization", tokenType+" "+tokenValue)
-	profileRes, profileResErr := profileClient.Do(profileReq)
-	if profileResErr != nil {
-		log.Fatal(profileResErr)
-		return nil
-	}
-
-	profileBody := ProfileBody{}
-	json.NewDecoder(profileRes.Body).Decode(&profileBody)
 
 	if profileBody.HasProfile {
 		fmt.Println("Email: " + profileBody.Profile.Email)
