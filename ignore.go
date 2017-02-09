a923019b9e67653704fb52ebed17a29385f17fba
Alex Miranda <alexmirandamoraes@gmail.com>
2017-02-09 14:12:40 +0100

    Fix glob patterns problems with appix watch

--- ignore.go
+++ ignore.go
@@ -10,15 +10,35 @@ import (
 	"github.com/ryanuber/go-glob"
 )
 
+// Supported glob patterns:
+// filename          match the filename in the root path, e.g. ./node_modules
+// */filename        match filename (depth: exactly 2), e.g. ./ui/node_modules
+// */*/filename      match filename (depth: exactly 3), e.g. ./ui/widget/node_modules
+// **/filename       match filename (depth: n | n > 0, i.e. does not include root), e.g. /any/depth/greather/than/0/node_modules
+// **/filename/*     match any file inside filename (depth > 0).  This is important because of watch
+
 var ignoredFileNames = []string{
-	"node_modules/*",
-	"temp/*",
-	".git/*",
-	".idea/*",
-	".vscode/*",
+	"node_modules",
+	"**/node_modules",
+	"**/node_modules/*",
+	".git",
+	".gitignore",
+	"**/.gitkeep",
+	"temp",
+	"npm-debug.log",
+	"**/npm-debug.log",
+	".idea",
+	"**/.idea",
+	"**/.idea/*",
+	".vscode",
+	"**/.vscode",
+	"**/.vscode/*",
 	".ds_store",
+	"**/.ds_store",
 	"thumbs.db",
+	"**/thumbs.db",
 	"desktop.ini",
+	"**/desktop.ini",
 	config.DevFileName,
 	config.IgnoreFileName,
 }
