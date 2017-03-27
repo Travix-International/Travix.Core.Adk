a57c7bcbd7814bf0e1d3370a8b2886dc2ee4fbfe
Jean Baudin <baudin.jean@gmail.com>
2017-03-27 08:35:14 +0200

    Hotfix remove private data (#63)
    remove private data from the source code

--- build.sh
+++ build.sh
@@ -16,7 +16,9 @@ APP_LDFLAGS="-s
 -X main.travixFirebaseMessagingSenderId=$TRAVIX_FIREBASE_MESSAGING_SENDER_ID
 -X main.travixFirebaseRefreshTokenUrl=$TRAVIX_FIREBASE_REFRESH_TOKEN_URL
 -X main.travixDeveloperProfileUrl=$TRAVIX_DEVELOPER_PROFILE_URL
--X main.travixLoggerUrl=$TRAVIX_LOGGER_URL"
+-X main.travixLoggerUrl=$TRAVIX_LOGGER_URL
+-X livereload.certContent=$TRAVIX_CERT_CONTENT
+-X livereload.keyContent=$TRAVIX_KEY_CONTENT"
 
 # run the tests
 go test $(go list ./... | grep -v /vendor/)
--- livereload/livereload.go
+++ livereload/livereload.go
@@ -2,6 +2,7 @@ package livereload
 
 import (
 	"bytes"
+	"encoding/base64"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -14,41 +15,19 @@ var (
 	livereloadAddressTLS = ":13222"
 )
 
-const (
-	certContent = `-----BEGIN CERTIFICATE-----
-MIICkzCCAfwCCQCbmnQ2PFatzzANBgkqhkiG9w0BAQsFADCBjTELMAkGA1UEBhMC
-TkwxEzARBgNVBAgMClNvbWUtU3RhdGUxEjAQBgNVBAcMCUFtc3RlcmRhbTEPMA0G
-A1UECgwGVHJhdml4MQ0wCwYDVQQLDARDb3JlMRIwEAYDVQQDDAlsb2NhbGhvc3Qx
-ITAfBgkqhkiG9w0BCQEWEm12aW5jemVAdHJhdml4LmNvbTAeFw0xNjEwMTUxNzEy
-NTVaFw0xOTA4MDUxNzEyNTVaMIGNMQswCQYDVQQGEwJOTDETMBEGA1UECAwKU29t
-ZS1TdGF0ZTESMBAGA1UEBwwJQW1zdGVyZGFtMQ8wDQYDVQQKDAZUcmF2aXgxDTAL
-BgNVBAsMBENvcmUxEjAQBgNVBAMMCWxvY2FsaG9zdDEhMB8GCSqGSIb3DQEJARYS
-bXZpbmN6ZUB0cmF2aXguY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCf
-OSB7LkkaRd6WXTplUHfD2k+EHoVi9flKcmbUlye9zHFzWVtCUQjhFjiZL1rNRQGn
-9VMUqzpc55RyzTEy2KpyZ+7INR1ZAuqqXMxpNDzXeq+UQuAFnJrHnbwtiSYPiJ45
-5EvysllYb5j6ihXEVZt+6QdMINFB+Gz0Xfrhug0+0QIDAQABMA0GCSqGSIb3DQEB
-CwUAA4GBADrH8ibFye3iXHR6RkwVNBgeKyvL0kxs4C8785uYqjRJWVjAg2xJQyyZ
-R3IHuvKqkmjs5i5d5CT9QT4t8Mlorg1XSnRz/HLf5zrRJlVzqrpd9N2+859TmTVD
-9A91NtEwCNgBSGDGSCndjQ/dkPhbJFs28/ICujLySxbYswOGHGbK
------END CERTIFICATE-----
-`
-	keyContent = `-----BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQCfOSB7LkkaRd6WXTplUHfD2k+EHoVi9flKcmbUlye9zHFzWVtC
-UQjhFjiZL1rNRQGn9VMUqzpc55RyzTEy2KpyZ+7INR1ZAuqqXMxpNDzXeq+UQuAF
-nJrHnbwtiSYPiJ455EvysllYb5j6ihXEVZt+6QdMINFB+Gz0Xfrhug0+0QIDAQAB
-AoGAIo9SxonwYhyCSN7peu4xYLh1A/df+m/rcUZNnZ1FigPjKCdgEI/oPnsFQ/Ks
-Ydu1lVBBfT4BSAMYDKcPI7s1m5Hf++2TAWXuE/GiMmfmQq8QHVwdRERIzGo7BSIW
-alA5tC4+dIe5gUKjR38MpG9VCEa3FBkNxlRQ2U1tIAoM9/ECQQDLWvbShPYpfKCM
-8WlAGeWwgHJrjdmatMLsJepxFjGShxK1uhLy6mIMaVVCV0dFPk2Y81ACAirmev99
-bqMd3sbtAkEAyHFgTZzQUrezQQhnfFcEDOaUrCwRBVERHFou6wHEwTLObJeedAuo
-emRRpQkOp+wJq8y9eOI2pv0jpSI8pTKW9QJAdOuzOG1sX4Qhh4gSHOIG90mTABYK
-BHJkFITkW+sHy5jQAB6hYHu0rjAt7jviZYSh9wwGd3Epm2Ui2sqvDLCXLQJBAKAk
-NNTNXIM50TU8CbIFs267Kj0EV/Tvd8Q3KRUJLLFObi3EVQxR5CEk1TYNrm/q3S8t
-PJO/5/oydLASUnGJoaECQGyPpJ6lVJb10yJKjcGtouwa+HFRJh9BxIQUHZRTbmHX
-k7iRrF0Vcllo8k/Mos5PVPP0WIyS1l0lh4GZ+w8gA80=
------END RSA PRIVATE KEY-----
-`
+var (
+	certContent string
+	keyContent  string
 )
+
+func base64Decode(src string) []byte {
+	val, err := base64.StdEncoding.DecodeString(certContent)
+
+	if err != nil {
+		log.Fatalf("An error occured while decoding the value. Details: %s\n", err.Error())
+	}
+	return val
+}
 
 func createCertFiles() (cert string, key string) {
 	tempFolder, _ := ioutil.TempDir("", "appix")
@@ -56,8 +35,8 @@ func createCertFiles() (cert string, key string) {
 	cert = tempFolder + "/livereload-cert.pem"
 	key = tempFolder + "/livereload-key.pem"
 
-	ioutil.WriteFile(cert, []byte(certContent), 0644)
-	ioutil.WriteFile(key, []byte(keyContent), 0644)
+	ioutil.WriteFile(cert, base64Decode(certContent), 0644)
+	ioutil.WriteFile(key, base64Decode(keyContent), 0644)
 
 	return cert, key
 }
