731a41d6300dcf92f0eb0fdbd46f5fda587178a7
Mark Vincze <mrk.vincze@gmail.com>
2017-07-03 16:09:26 +0200

    Add a helper script to generate the Firebase config env vars (#78)

---
+++ firebase-config-helper.js
@@ -0,0 +1,31 @@
+// A helper script to quickly generate the necessary env variables.
+// Copy here the Json configuration from the Firebase console, and run this script in a REPL.
+var config = {
+    apiKey: "DummyApiKey",
+    authDomain: "fireball-development.firebaseapp.com",
+    databaseURL: "https://fireball-development.firebaseio.com",
+    projectId: "fireball-development",
+    storageBucket: "fireball-development.appspot.com",
+    messagingSenderId: "445151709427"
+  };
+
+console.log(
+`For PowerShell:
+$env:TRAVIX_FIREBASE_API_KEY='${config.apiKey}'
+$env:TRAVIX_FIREBASE_DATABASE_URL='${config.databaseURL}'
+$env:TRAVIX_FIREBASE_STORAGE_BUCKET='${config.storageBucket}'
+$env:TRAVIX_FIREBASE_MESSAGING_SENDER_ID='${config.messagingSenderId}'
+$env:TRAVIX_FIREBASE_AUTH_DOMAIN='${config.authDomain}'
+$env:TRAVIX_FIREBASE_REFRESH_TOKEN_URL='https://securetoken.googleapis.com/v1/token?key='
+$env:TRAVIX_DEVELOPER_PROFILE_URL='https://developerprofile.${config.projectId.endsWith("development") ? "development." : config.projectId.endsWith("staging") ? "staging." : ""}travix.com/'
+$env:TRAVIX_LOGGER_URL='https://frogger.travix.com/'
+
+For bash:
+TRAVIX_FIREBASE_API_KEY='${config.apiKey}'
+TRAVIX_FIREBASE_DATABASE_URL='${config.databaseURL}'
+TRAVIX_FIREBASE_STORAGE_BUCKET='${config.storageBucket}'
+TRAVIX_FIREBASE_MESSAGING_SENDER_ID='${config.messagingSenderId}'
+TRAVIX_FIREBASE_AUTH_DOMAIN='${config.authDomain}'
+TRAVIX_FIREBASE_REFRESH_TOKEN_URL='https://securetoken.googleapis.com/v1/token?key='
+TRAVIX_DEVELOPER_PROFILE_URL='https://developerprofile.${config.projectId.endsWith("development") ? "development." : config.projectId.endsWith("staging") ? "staging." : ""}travix.com/'
+TRAVIX_LOGGER_URL='https://frogger.travix.com/'`);
